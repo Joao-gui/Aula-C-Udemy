f4c92deb143b9dd5f744df1f4c8af9941ad1aae1
João <eng.joaopellacani@gmail.com>
2023-06-12 17:02:31 -0300

    fix: Fix C to K equation

--- Exercicios Secao3/E8.c
+++ Exercicios Secao3/E8.c
@@ -2,7 +2,7 @@
  * @file E8.c
  * @author your name (you@domain.com)
  * @brief Leia uma temperatura em Kelvin e apresente-a convertida em Celsius.
- * A fórmula de conversão é: C=K+372.15
+ * A fórmula de conversão é: C=K-273.15
  * @version 0.1
  * @date 2023-06-12
  * 
@@ -17,7 +17,7 @@ int main()
     double temperatucaC, temperatudaK;
     printf("Digite a temperatura em Kelvin: ");
     scanf("%lf", &temperatudaK);
-    temperatucaC=temperatudaK+372.15;
+    temperatucaC=temperatudaK-273.15;
     printf("A temperatura %.2lfK em Celsius e de %.2lfC", temperatudaK, temperatucaC);
     return 0;
 }
