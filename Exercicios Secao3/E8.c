5c860280d236af420e18f56eaf67b649fa9785e4
João <eng.joaopellacani@gmail.com>
2023-06-12 16:54:23 -0300

    feat: Exercise 8

---
+++ Exercicios Secao3/E8.c
@@ -0,0 +1,23 @@
+/**
+ * @file E8.c
+ * @author your name (you@domain.com)
+ * @brief Leia uma temperatura em Kelvin e apresente-a convertida em Celsius.
+ * A fórmula de conversão é: C=K+372.15
+ * @version 0.1
+ * @date 2023-06-12
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    double temperatucaC, temperatudaK;
+    printf("Digite a temperatura em Kelvin: ");
+    scanf("%lf", &temperatudaK);
+    temperatucaC=temperatudaK+372.15;
+    printf("A temperatura %.2lfK em Celsius e de %.2lfC", temperatudaK, temperatucaC);
+    return 0;
+}
