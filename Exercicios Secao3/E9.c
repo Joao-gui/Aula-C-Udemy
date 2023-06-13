dc3f8bcf5455f17b58d13d010ea3f8cafd3a5907
João <eng.joaopellacani@gmail.com>
2023-06-13 13:00:43 -0300

    fix: Acentuação

--- Exercicios Secao3/E9.c
+++ Exercicios Secao3/E9.c
@@ -2,7 +2,7 @@
  * @file E9.c
  * @author your name (you@domain.com)
  * @brief Leia uma temperatura em Celsius e apresente-a convertida em Kelvin.
- * A formula de convers√£o √©: K=C+273.15
+ * A formula de convers„o È: K=C+273.15
  * @version 0.1
  * @date 2023-06-12
  * 
@@ -11,13 +11,17 @@
  */
 
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
     double temperaturaC, temeperaturaK;
+
     printf("Digite a temperatura em Celsius: ");
     scanf("%lf", &temperaturaC);
     temeperaturaK=temperaturaC+273.15;
-    printf("A temperatura %.2lfC em Kelvin e de %.2lfK", temperaturaC, temeperaturaK);
+    printf("A temperatura %.2lfC em Kelvin È de %.2lfK", temperaturaC, temeperaturaK);
     return 0;
 }
